c4efcc43a762bc5adc45a60ab3bcbe6b333459ab
appyouz <dikpalkhatri7@gmail.com>
2024-02-24 21:22:06 +0545

    Add code to print AES algorithm information
    This commit adds a simple C++ program that prints out information about the AES (Advanced Encryption Standard) algorithm provided by the Crypto++ library. The program prints the default key length, minimum key length, maximum key length, and block size of the AES algorithm.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
+#include <cryptopp/aes.h>
+#include <cryptopp/cryptlib.h>
 #include <iostream>
 
+using namespace CryptoPP;
 int main(int argc, char *argv[]) {
-  std::cout << "FILE ENCRYPTION TOOL\n";
+
+
+  std::cout << "key length: " << AES::DEFAULT_KEYLENGTH << std::endl;
+  std::cout << "key length (min): " << AES::MIN_KEYLENGTH << std::endl;
+  std::cout << "key length (max): " << AES::MAX_KEYLENGTH << std::endl;
+  std:: cout << "block size: " << AES::BLOCKSIZE << '\n';
   return 0;
 }
